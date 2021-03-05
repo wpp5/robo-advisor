b030be3786abe4a792024cccb7d1615de860a46c
wpp5 <wpp5@georgetown.edu>
2021-03-05 14:34:01 -0500

    Latest Day/Close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,6 +7,26 @@ import requests
 
 from dotenv import load_dotenv
 
+#
+# INFO INPUTS
+#
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+response = requests.get(request_url)
+
+#print(type(response))
+#print(response.status_code)
+#print(response.status_code)
+#print(response.text)
+
+
+
+seg_response = json.loads(response.text)
+last_refreshed = seg_response["Meta Data"]["3. Last Refreshed"]
+latest_close = seg_response["Time Series (Daily)"]["2021-03-04"]["4. close"]
+
+def to_usd(my_price):
+    return f"${my_price:,.2f}" #> $12,000.71
 
 
 
@@ -16,8 +36,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
