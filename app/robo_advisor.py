2de978741d4833be559bf482eee56265c37f6874
wpp5 <wpp5@georgetown.edu>
2021-03-08 14:59:26 -0500

    Adding Comments and Cleaning Up Data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,7 @@
 # this is the "app/robo_advisor.py" file
+# The code will take a ticket symbol from the user and return various information including a buy/don't buy reccomendation 
 
+#Importing packages and modules
 import os
 import csv
 import json
@@ -7,31 +9,41 @@ import requests
 import datetime
 import time
 
+#Time delay to simulate computer thinking
+delay = 1.5
+
+
 from dotenv import load_dotenv
 
+#loads .env file
 load_dotenv()
 
-#
-# INFO INPUTS
-#
-
+#variable assignmen for unique API key
 apikey = os.environ.get("ALPHAVANTAGE_API_KEY")
+
 while True:
+    #Data Validation for incorrect ticket entry
     try: 
         symbol = input("Enter stock symbol for analysis. For example, AAPL, JNJ, etc.").upper()
+        #Variable assignment to URL for ticker data
         request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={apikey}"
+
+        #Requests URL data
         response = requests.get(request_url)
-
+        #Loads URL text
         seg_response = json.loads(response.text)
-
-        last_refreshed = seg_response["Meta Data"]["3. Last Refreshed"]
+        #assigns variable to get into library
+        tsd = seg_response["Time Series (Daily)"]
         break
     except:
+        time.sleep(delay)
         print("Hmm...it appears that you did not enter a valid symbol! Try again")
 
-tsd = seg_response["Time Series (Daily)"]
+#Assigns the dates into a list called dates
+dates = list(tsd.keys())
 
-dates = list(tsd.keys()) # TODO: Sort so latest day is first
+#Sorts dates from most recent in case data format changes
+dates.sort(reverse=True)
 
 latest_day = dates[0]
 
@@ -39,37 +51,42 @@ latest_close = tsd[latest_day]["4. close"]
 
 high_prices = []
 low_prices = []
-#maximum of all high prices
+
+#Loops from to find high and low prices for each day and appends them to the respective dictionaries
 for date in dates:
     high_price = tsd[date]["2. high"]
     low_price = tsd[date]["3. low"]
     high_prices.append(float(high_price))
     low_prices.append(float(low_price))
 
-
+#Returns high and low prices in the list
 recent_high = max(high_prices)
 recent_low = min(low_prices)
 
-
+#Buy logic 
+#Will recommend buy if latest close is between 60% and 80% of the recent high price
 if float(latest_close) >= .6*recent_high and float(latest_close) <= .8*recent_high:
     buy_rec = "BUY!" 
     rec_reason = "STOCK PRICE IS BETWEEN 60% AND 80% OF RECENT HIGH."
 
+#Will reccomend don't buy if the price does not meet the aforementioned conditions
 else: 
     buy_rec = "DON'T BUY!"
     rec_reason = "PRICE IS NOT WITHIN THE ACCEPTABLE RANGE OF RECENT HIGH."
 
+#to_usd converts non-formatted numbers to USD currency format
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
-#csv_file_path = "data/prices.csv" # a relative filepath
-
+#Puts the prices.csv file into the data folder in the robo-advisor repo
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+#Name of the headers for the CSV file
 csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+#Writes the values for the csv headers into the CSV file by looping through the days
 with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
     writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader() # uses fieldnames set above
-    #looping
     for date in dates:
         daily_prices = tsd[date]
         writer.writerow({
@@ -81,14 +98,19 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
             "volume": daily_prices["5. volume"]
         })
 
+
+##Info Outputs
+
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
+time.sleep(delay)
+#Uses the datatime function to return the current tume of the request
 now = datetime.datetime.now()
 print("REQUEST AT:", now.strftime("%Y-%m-%d %I:%M %p"))
 print("-------------------------")
-print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST DAY: {latest_day}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
@@ -97,6 +119,7 @@ print(f"RECOMMENDATION: {buy_rec}")
 print(f"RECOMMENDATION REASON: {rec_reason}")
 print("-------------------------")
 print(f"WRITING DATA TO CSV: {csv_file_path}...")
+time.sleep(delay)
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-----")
